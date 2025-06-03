175bf22d798b596fd26b6e6e939cb46565f52af3
AnthonyLubin <toyne37@gmail.com>
2025-06-03 15:04:18 -0400

    Update image_processing.cpp
    fixed horizontal flip

--- src/image_processing.cpp
+++ src/image_processing.cpp
@@ -188,13 +188,20 @@ Image *apply_blur_filter(const Image *input_image) {
     return output_image;
 }
 
-void fliphorizontal(const Image *input_image){
+void fliphorizontal(Image *input_image){
     int w = input_image->width;
     int h = input_image->height;
-    for(int i = 0; w<h; i++){
-        for(int j = 0; j<w/2; j++){
-            std::swap(Image_Data[x][y], Image_Data[w-1-x][y]);
+    int Image_Data = input_image->data;
+    int c = input_image->channels;
+    for(int y = 0; y<h; ++i){
+        for(int x = 0; x<w/2; ++j){
+            for(int ch = 0; ch<c; ++ch){
+                int left_idx = (y*w+x)*c + ch;
+                int right_idx = (y*w+(w-1-x)) * c + ch; 
+                std::swap(Image_Data[left_idx], Image_Data[right_idx);
         }
+        }
+    }
 
 // Function to detect edges (Sobel operator)
 Image *detect_edges(const Image *input_image) {
